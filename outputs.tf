3b040ffe771828e135635eda3dbf26b595bbe178
prageethpasyala <prageeth.pasyala@cloudreach.com>
2022-02-19 12:51:05 +0000

    edit output file

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 // Output the ID of the EC2 instance created
 output "ec2_instance_id" {
-  value = "${aws_instance.ec2_instance.id}"
+  value = "${aws_instance.ec2_instance[*].id}"
 }
 
 output "ec2_instance_pub_ip"{
-    value = aws_instance.ec2_instance.public_ip
+    value = aws_instance.ec2_instance[*].public_ip
 }
